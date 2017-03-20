e641c1e4e2cf324fb610947d77366d561c0d4839
Matt <matthiasaion@gmail.com>
2017-03-20 14:18:37 +0100

    Zoom on picture ok

--- app/src/main/java/com/he/matt/wheresaldo/ui/GameOneActivity.java
+++ app/src/main/java/com/he/matt/wheresaldo/ui/GameOneActivity.java
@@ -3,13 +3,17 @@ package com.he.matt.wheresaldo.ui;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.chrisbanes.photoview.PhotoViewAttacher;
 import com.he.matt.wheresaldo.R;
 
 public class GameOneActivity extends AppCompatActivity {
 
     private TextView countDown;
+    private ImageView image;
+    private PhotoViewAttacher mAttacher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,10 @@ public class GameOneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game_one);
 
         countDown = (TextView) findViewById(R.id.countDown);
+        image = (ImageView) findViewById(R.id.imageView2);
+        image.setImageResource(R.drawable.cage1);
+
+        mAttacher = new PhotoViewAttacher(image);
 
         new CountDownTimer(30000, 1000) {
 
